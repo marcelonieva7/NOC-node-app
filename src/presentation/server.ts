f4946dbc3ba95489b16c1f6c23957ab468e2ec2c
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-06 21:41:16 -0300

    feat: integrate CronService and CheckService for periodic URL checks on server start

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,21 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
 export class ServerApp {
   constructor() {
-      
   }
 
   static start() {
     console.log("Server is starting...");
+
+    CronService.createJob(
+      "*/5 * * * * *",
+      () => {
+        const checkService = new CheckService();
+        const url = "https://cursos.devtalles.com/";
+        checkService.executeCheck(url)
+    });
+
+    console.log("Server started successfully.");
   }
 }
