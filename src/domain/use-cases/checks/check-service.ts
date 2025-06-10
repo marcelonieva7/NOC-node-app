aac3dab3bd08396b8a8bc7a3f30ccf92ff4ebccc
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-10 19:18:49 -0300

    feat: integrate logging into CheckService for success and error handling

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,8 @@
+import {
+  LogRepository as LogRepositoryAbstract
+} from '../../repositories/log.repository';
+import { LogEntity, LogSecurityLevelEnum } from '../../entities/log.entity';
+
 interface CheckServiceUseCase {
   executeCheck(url: string): Promise<boolean>;
 }
@@ -17,15 +22,26 @@ export class CheckService implements CheckServiceUseCase {
     try {
        const req = await fetch(url)
       if (!req.ok) {
-        throw new Error(`Error on check service: ${url}`);
+        throw new Error(`Error on check service: ${req.status} ${req.statusText}`);
       }
 
+      const log = new LogEntity(
+        LogSecurityLevelEnum.low,
+        `Check service executed successfully: ${url}`
+      );
+      this.logRepository.saveLog(log)
       this.successCallback()
 
       return true;
     } catch (error) {
-      this.errorCallback(String(error))
-      console.error(error);
+      const errorStr = 
+        `Error on check service ${url} : ${error}`;
+      this.errorCallback(errorStr)
+      const log = new LogEntity(
+        LogSecurityLevelEnum.high,
+        errorStr
+      );
+      this.logRepository.saveLog(log)
 
       return false;      
     }
