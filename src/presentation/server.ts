fa51a79368b174feb8b234bb6c9e00f1a0a4d6ea
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-10 19:22:07 -0300

    feat: add logging functionality to ServerApp with LogRepository and FileSystemDataSource

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { LogRepository } from "../infrastructure/repositories/log-repository";
+import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { CronService } from "./cron/cron-service";
+
+const logRepository = new LogRepository(
+  new FileSystemDataSource()
+);
 
 export class ServerApp {
   constructor() {
@@ -11,8 +17,9 @@ export class ServerApp {
     CronService.createJob(
       "*/5 * * * * *",
       () => {
-        const url = "https://cursos.devtalles.com/";
+        const url = "https://cursos.devtalles.com/courses/";
         const checkService = new CheckService(
+          logRepository,
           () => console.log(`${url} is OK`),
           error => console.error(error)          
         );
