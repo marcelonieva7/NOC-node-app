e326d3942268264222085ac1f572483ce8e21f1c
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-11 20:20:12 -0300

    ref: update .env.template to include MAILER_SERVICE and adjust MAILER_EMAIL

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -3,6 +3,7 @@ import * as env from 'env-var';
 
 export const envsPlugin = {
   PORT: env.get('PORT').required().asPortNumber(),
+  MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
   MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
   MAILER_PASSWORD: env.get('MAILER_PASSWORD').required().asString(),
   PROD: env.get('PROD').required().asBool(),
