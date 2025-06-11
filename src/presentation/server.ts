bc6c7d95632a862a6da1b41021dda7af12fe361c
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-11 20:25:01 -0300

    feat: integrate EmailService and SendLogsEmail use case into ServerApp

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,14 +2,22 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { LogRepository } from "../infrastructure/repositories/log-repository";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email-service";
+import { SendLogsEmail } from "../domain/use-cases/email/send-logs";
 
 const logRepository = new LogRepository(
   new FileSystemDataSource()
 );
+const emailService = new EmailService();
+const sendLogsEmail = new SendLogsEmail(logRepository, emailService);
 
 export class ServerApp {
   constructor() {
   }
+
+/*   static async start() {
+    await sendLogsEmail.execute(['marcelonieva7@gmail.com']);
+  } */
 
   static start() {
     console.log("Server is starting...");
@@ -20,8 +28,8 @@ export class ServerApp {
         const url = "https://cursos.devtalles.com/courses/";
         const checkService = new CheckService(
           logRepository,
-          () => console.log(`${url} is OK`),
-          error => console.error(error)          
+          /* () => console.log(`${url} is OK`),
+          error => console.error(error)      */     
         );
         checkService.executeCheck(url)
     });
