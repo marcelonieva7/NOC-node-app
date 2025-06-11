0a6f135a3de8d70a43ebe612c7c2aa1c95add8f4
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-11 20:22:43 -0300

    ref: change import to use type for LogEntity and LogSeverityLevel in log.repository.ts

--- src/domain/repositories/log.repository.ts
+++ src/domain/repositories/log.repository.ts
@@ -1,4 +1,4 @@
-import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
+import type { LogEntity, LogSeverityLevel } from "../entities/log.entity";
 
 export abstract class LogRepository {
   abstract saveLog(log: LogEntity): Promise<void>;
