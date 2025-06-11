20afe31105b61f324f3401542cf12d4c6624d579
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-11 00:43:32 -0300

    ref: enhance LogEntity to include origin and createdAt properties in constructor

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -6,20 +6,44 @@ export const LogSecurityLevelEnum = {
   high: 'high'
 } as const satisfies Record<string, string>;
 
+export type LogEntityOptions = {
+  level: LogSeverityLevel;
+  message: string;
+  createdAt?: Date;
+  origin: string;
+}
+
+export type LogEntityJson = Omit<LogEntity, 'createdAt'> & {
+  createdAt: string;
+};
+
 export class LogEntity {
   public createdAt: Date;
+  public level: LogSeverityLevel;
+  public message: string;
+  public origin: string;
 
-  constructor(
-    public level: LogSeverityLevel,
-    public message: string
-  ) {
-    this.createdAt = new Date();
+  constructor({ level, message, createdAt = new Date(), origin }: LogEntityOptions) {
+    this.createdAt = createdAt;
+    this.level = level;
+    this.message = message;
+    this.origin = origin;
   }
 
-  private static validate(data: unknown): data is LogEntity {
+  private static validate(data: unknown): data is LogEntityJson {
     if (typeof data !== 'object' || data === null) return false;
-    if (!('level' in data) || !('message' in data)) return false;
-    if (typeof data.level !== 'string' || typeof data.message !== 'string') return false;
+    if (
+      !('level' in data) ||
+      !('message' in data) ||
+      !('origin' in data) ||
+      !('createdAt' in data)
+    ) return false;
+    if (
+      typeof data.level !== 'string' ||
+      typeof data.message !== 'string' ||
+      typeof data.origin !=='string' ||
+      typeof data.createdAt !=='string'
+    ) return false;
     if (!Object.keys(LogSecurityLevelEnum).includes(data.level)) return false;
 
     return true;
@@ -30,8 +54,13 @@ export class LogEntity {
     if (!LogEntity.validate(data)) {
       throw new Error('Invalid log data');
     }
-    const newLog = new LogEntity(data.level, data.message);
-    newLog.createdAt = new Date(data.createdAt);
+    const { level, message, createdAt, origin } = data;
+    const newLog = new LogEntity({
+      level,
+      message,
+      origin,
+      createdAt: new Date(createdAt)
+    });
     
     return newLog;
   }
