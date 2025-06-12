f060199d024bba37c700c0a47334e6d3c873d682
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-11 21:53:11 -0300

    feat: add MongoDB configuration to environment variables

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -7,4 +7,8 @@ export const envsPlugin = {
   MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
   MAILER_PASSWORD: env.get('MAILER_PASSWORD').required().asString(),
   PROD: env.get('PROD').required().asBool(),
+  MONGO_URL: env.get('MONGO_URL').required().asString(),
+  MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
+  MONGO_USER: env.get('MONGO_USER').required().asString(),
+  MONGO_PASSWORD: env.get('MONGO_PASSWORD').required().asString(),
 }
