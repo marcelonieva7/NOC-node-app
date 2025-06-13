5ca221eb2788dffcfad9be4df67fce12bc9789fe
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-13 01:56:06 -0300

    feat: establish MongoDB connection in main application entry point

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
+import { envsPlugin } from "./config/plugins/envs.plugin";
+import { MongoDB } from "./infrastructure/database/mongo/init";
 import { ServerApp } from "./presentation/server"
 
 const main = async () => {
+  await MongoDB.connect({
+    dbName: envsPlugin.MONGO_DB_NAME,
+    uri: envsPlugin.MONGO_URL,
+  })
+
   ServerApp.start();
 }
 
