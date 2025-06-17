26ae991860f120c78ec489133e1dd4efcb099a78
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-17 04:21:28 -0300

    test: add unit test for CronService to validate cron job creation

--- test/presentation/cron/cron-service.test.ts
+++ test/presentation/cron/cron-service.test.ts
@@ -1,7 +1,15 @@
-import {} from '../../../src/presentation/cron/cron-service';
+import { CronService } from '../../../src/presentation/cron/cron-service';
+import { CronJob } from "cron";
 
 describe('cron-service.ts', () => {
-  test('should ', () => {
-    // TODO
+  test('should create cron job', () => {
+    const spyCronJob = jest.spyOn(CronJob.prototype, 'start').mockImplementation(() => {});
+    const mockCallback = jest.fn();
+    const cronExpression = '* * * * * *';
+    const job = CronService.createJob(cronExpression, mockCallback);
+
+    expect(spyCronJob).toHaveBeenCalledTimes(1);
+    expect(job).toBeInstanceOf(CronJob);
+    
   });
 });
