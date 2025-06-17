d21d95fcfc8644dc442db6e4376eb6146156afb8
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-17 03:59:10 -0300

    test: add unit tests for LogRepository to validate log saving and retrieval

--- test/infrastructure/repositories/log-repository.test.ts
+++ test/infrastructure/repositories/log-repository.test.ts
@@ -1,7 +1,38 @@
-import {} from '../../../src/infrastructure/repositories/log-repository';
+import { LogRepository } from '../../../src/infrastructure/repositories/log-repository';
+import type { LogDataSource } from "../../../src/domain/datasources/log.datasource";
+import { LogEntity, LogSecurityLevelEnum } from '../../../src/domain/entities/log.entity';
 
 describe('log-repository.ts', () => {
-  test('should ', () => {
-    // TODO
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
+
+  const mockDataSource: LogDataSource = {
+    saveLog: jest.fn(),
+    getLogs: jest.fn().mockResolvedValue([])
+  };
+
+  const logRepository = new LogRepository(mockDataSource);
+
+  const newLog: LogEntity = {
+    level : LogSecurityLevelEnum.high,
+    message: "test message",
+    origin: "test origin",
+    createdAt: new Date()
+  }
+  
+  test('should save log correctly', async() => {
+    await mockDataSource.saveLog(newLog)
+
+    expect(mockDataSource.saveLog).toHaveBeenNthCalledWith(1, newLog);
+    expect(logRepository.saveLog(newLog)).resolves.toBeUndefined();
+  });
+
+  test('should get logs correctly', async() => {
+    await mockDataSource.getLogs(LogSecurityLevelEnum.high)
+
+    expect(mockDataSource.getLogs).toHaveBeenNthCalledWith(1, LogSecurityLevelEnum.high);
+    expect(logRepository.getLogs(LogSecurityLevelEnum.high)).resolves.toEqual([]);
+    
+  })
 });
