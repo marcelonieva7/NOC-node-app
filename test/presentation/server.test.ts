5d6784499caac98338b2c5a8cddd1a8acb0499f6
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-19 17:07:09 -0300

    test: add unit test for ServerApp to validate server start functionality

--- test/presentation/server.test.ts
+++ test/presentation/server.test.ts
@@ -1,7 +1,15 @@
-import {} from '../../src/presentation/server';
+import { ServerApp, startMessage } from '../../src/presentation/server';
+import { CronService } from "../../src/presentation/cron/cron-service";
+
+jest.mock('../../src/presentation/cron/cron-service');
 
 describe('server.ts', () => {
-  test('should ', () => {
-    // TODO
+  const consoleLogSpy = jest
+    .spyOn(console, 'log')
+    .mockImplementation(() => {});
+  test('start should start server ', () => {
+    expect(()=>ServerApp.start()).not.toThrow();
+    expect(CronService.createJob).toHaveBeenCalledTimes(1);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(1, startMessage);
   });
 });
