85ed443a066106a4a430d5e2d8264bf154ac6c82
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-19 17:06:18 -0300

    refactor: export main function and handle error separately in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { envsPlugin } from "./config/plugins/envs.plugin";
 import { MongoDB } from "./infrastructure/database/mongo/init";
 import { ServerApp } from "./presentation/server"
 
-const main = async () => {
+export const main = async () => {
   await MongoDB.connect({
     dbName: envsPlugin.MONGO_DB_NAME,
     uri: envsPlugin.MONGO_URL,
@@ -11,7 +11,9 @@ const main = async () => {
   ServerApp.start();
 }
 
-main().catch((error) => {
-  console.error("An error occurred:", error);
-  process.exit(1);
-});
+export const handleError = (error: unknown) => {
+    console.error("An error occurred:", error);
+    process.exit(1);
+}
+
+main().catch(handleError);
