35722a0d5f1fc7a884718afe528b8096a987892b
Marcelo Nieva <marcelonieva7@gmail.com>
2025-06-19 17:07:01 -0300

    test: add unit tests for PostgresLogDataSource to validate log saving and retrieval functionality

--- test/infrastructure/datasources/postgres-log.datasource.test.ts
+++ test/infrastructure/datasources/postgres-log.datasource.test.ts
@@ -1,7 +1,42 @@
-import {} from '../../../src/infrastructure/datasources/postgres-log.datasource';
+
+import { PostgresLogDataSource } from '../../../src/infrastructure/datasources/postgres-log.datasource';
+
+jest.mock('../../../src/generated/prisma', () => ({
+  PrismaClient: jest.fn(() => ({
+    log: {
+      create: jest.fn(({level})=> ({
+        level,
+        message: 'test message',
+        origin: 'test origin',
+        createdAt: new Date()
+      })),
+      findMany: jest.fn(()=> [newLog, newLog])
+    }
+  })),
+  SecurityLevel: {
+    HIGH: 'high',
+    MEDIUM: 'medium',
+    LOW: 'low'
+  } 
+}));
+
+const newLog = {
+  level: 'high',
+  message: 'test message',
+  origin: 'test origin',
+  createdAt: new Date()
+} as const
 
 describe('postgres-log.datasource.ts', () => {
-  test('should ', () => {
-    // TODO
+  const postgresLogDS = new PostgresLogDataSource();
+
+  test('should save log correctly', async () => {
+    expect(postgresLogDS.saveLog(newLog)).resolves.toBeUndefined();
   });
+
+  test('should get all logs correctly', () => {
+    const getLogsPromise = postgresLogDS.getLogs('low')
+    
+    expect(getLogsPromise).resolves.toEqual([newLog, newLog]);
+  })
 });
